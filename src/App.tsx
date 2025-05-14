bbad71c0a70c39062717a086fc03e192ff939672
Nathan Baleeta <nbaleeta@gmail.com>
2025-05-14 05:32:03 -0700

    use context api to manage access and csrf tokens globally

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import * as React from 'react';
+import React, { createContext, useState, useEffect } from "react";
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PersonIcon from '@mui/icons-material/Person';
 import { Outlet } from 'react-router';
 import { ReactRouterAppProvider } from '@toolpad/core/react-router';
 import type { Navigation } from '@toolpad/core/AppProvider';
+
 
 const NAVIGATION: Navigation = [
   {
@@ -14,18 +15,83 @@ const NAVIGATION: Navigation = [
     title: 'Dashboard',
     icon: <DashboardIcon />,
   },
-  {
-    segment: 'employees',
-    title: 'Employees',
-    icon: <PersonIcon />,
-    pattern: 'employees{/:employeeId}*',
-  },
 ];
 
 const BRANDING = {
   title: "Dashboard Cloner",
 };
 
+// https://stackoverflow.com/questions/78114219/property-env-does-not-exist-on-type-importmeta-ts2339
+const SUPERSET_ENDPOINT = import.meta.env.VITE_SUPERSET_ENDPOINT; 
+const SECURITY_LOGIN_ENDPOINT: string = `${SUPERSET_ENDPOINT}/api/v1/security/login`;
+const SECURITY_API_CSRF_ENDPOINT: string = `${SUPERSET_ENDPOINT}/api/v1/security/csrf_token/`;
+
+const SUPERSET_ADMIN_USERNAME = import.meta.env.VITE_SUPERSET_ADMIN_USERNAME;
+const SUPERSET_ADMIN_PASSWORD = import.meta.env.VITE_SUPERSET_ADMIN_PASSWORD
+
+export const AuthContext = React.createContext({ accessToken: "", csrfToken: "" });
+
+const AuthProvider = ({ children }) => {
+  const [accessToken, setAccessToken] = useState(""); 
+  const [csrfToken, setCsrfToken] = useState(""); 
+
+  const getAccessToken = async () => {
+    const response = await fetch(SECURITY_LOGIN_ENDPOINT, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: SUPERSET_ADMIN_USERNAME,
+        password: SUPERSET_ADMIN_PASSWORD,
+        provider: 'db',
+        refresh: true
+      })
+    })
+    const result = await response.json();
+    setAccessToken(result['access_token'])
+    sessionStorage.setItem('accessToken', result['access_token'])
+  }
+
+  const getCSRFToken = async () => {
+    const ACCESS_TOKEN = sessionStorage.getItem('accessToken')
+    const response = await fetch(SECURITY_API_CSRF_ENDPOINT, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Authorization': 'Bearer ' + ACCESS_TOKEN
+      }
+    })
+    const data = await response.json();
+    setCsrfToken(data['result'])
+  }
+
+  useEffect(() => {
+    getAccessToken(),
+    getCSRFToken()
+  }, [SECURITY_LOGIN_ENDPOINT, SECURITY_API_CSRF_ENDPOINT]);
+
+  useEffect(() => {
+    if (accessToken) {
+      sessionStorage.setItem('accessToken', accessToken); // Store in sessionStorage whenever token changes
+    } else {
+      sessionStorage.removeItem('accessToken'); // Clear from sessionStorage if token is null
+    }
+    if (csrfToken) {
+      sessionStorage.setItem('csrfToken', csrfToken); // Store in sessionStorage whenever token changes
+    } else {
+      sessionStorage.removeItem('csrfToken'); // Clear from sessionStorage if token is null
+    }
+  }, [accessToken, csrfToken ]);
+
+  
+  return (
+    <AuthContext.Provider value={{ accessToken, csrfToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export default function App() {
   
